4f663f89314276d15b040f454027549b0206b418
Will <willusher.life@gmail.com>
2014-04-16 21:49:34 -0700

    Migrating batching into a more convenient class that manages it
    Later plan to template the class so it can have any desired attributes
    for the objects. Should also be able to toggle whether it's using a
    model or not.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,69 @@ struct Position : entityx::Component<Position> {
 	Position(float x, float y) : x(x), y(y) {}
 };
 
+//Simple render batch with a fixed capacity. Each instance only has a distinct model matrix
+class RenderBatch {
+	size_t size, capacity;
+	//Should also add a vbo here to hold the model being drawn
+	//but since I'm generating the triangles in the shader skip for now
+	GLuint vao, matrices, program;
+
+public:
+	RenderBatch(size_t capacity, GLuint program)
+		: size(0), capacity(capacity), vao(0), matrices(0), program(program){
+		//Defer creating buffers until an object is added
+	}
+	~RenderBatch(){
+		if (vao != 0){
+			glDeleteVertexArrays(1, &vao);
+			glDeleteBuffers(1, &matrices);
+		}
+		glDeleteProgram(program);
+	}
+	//Add a list of objects to be drawn
+	void add_objects(const std::vector<glm::mat4> &objs){
+		if (size + objs.size() > capacity){
+			std::cerr << "Too many objects to insert into batch, aborting\n";
+			return;
+		}
+		if (vao == 0){
+			create_buffer();
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, matrices);
+		glm::mat4 *mats = static_cast<glm::mat4*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+		for (size_t i = 0; i < objs.size(); ++i){
+			mats[i + size] = objs[i];
+		}
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+		size += objs.size();
+	}
+	//Set the attribute index to send matrices too
+	void set_attrib_index(unsigned attrib){
+		glBindVertexArray(vao);
+		for (unsigned i = attrib; i < attrib + 4; ++i){
+			glEnableVertexAttribArray(i);
+			glVertexAttribPointer(i, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4),
+					(void*)(sizeof(glm::vec4) * i));
+			glVertexAttribDivisor(i, 1);
+		}
+	}
+	//Render the batch
+	void render(){
+		glUseProgram(program);
+		glBindVertexArray(vao);
+		glDrawArraysInstanced(GL_TRIANGLES, 0, 3, size);
+	}
+
+private:
+	void create_buffer(){
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		glGenBuffers(1, &matrices);
+		glBindBuffer(GL_ARRAY_BUFFER, matrices);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * capacity, NULL, GL_STREAM_DRAW);
+	}
+};
+
 int main(int argc, char **argv){
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		std::cerr << "SDL_Init error: " << SDL_GetError() << "\n";
@@ -46,38 +109,27 @@ int main(int argc, char **argv){
 	glDebugMessageControlARB(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0,
 		NULL, GL_TRUE);
 
-	GLuint vao, vbo;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * 2, NULL, GL_STATIC_DRAW);
-	glm::mat4 *mats = static_cast<glm::mat4*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
-	mats[0] = glm::translate<GLfloat>(-0.5f, 0.f, 0.f) * glm::scale<GLfloat>(0.5f, 0.5f, 1.f);
-	mats[1] = glm::translate<GLfloat>(0.5f, 0.f, 0.f) * glm::scale<GLfloat>(0.5f, 0.5f, 1.f);
-	glUnmapBuffer(GL_ARRAY_BUFFER);
-
-	//Enable all columns of the matrices
-	for (int i = 0; i < 4; ++i){
-		glEnableVertexAttribArray(i);
-		glVertexAttribPointer(i, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4),
-			(void*)(sizeof(glm::vec4) * i));
-		glVertexAttribDivisor(i, 1);
-	}
-
 	GLint program = util::load_program({std::make_tuple(GL_VERTEX_SHADER, "../res/vertex.glsl"),
 		std::make_tuple(GL_FRAGMENT_SHADER, "../res/fragment.glsl")});
 	if (program == -1){
-		glDeleteVertexArrays(1, &vao);
 		SDL_GL_DeleteContext(context);
 		SDL_DestroyWindow(win);
 		SDL_Quit();
 		return 1;
 	}
-	glUseProgram(program);
+	RenderBatch batch(4, program);
+	std::vector<glm::mat4> matrices = {
+		glm::translate<GLfloat>(-0.5f, 0.f, 0.f) * glm::scale<GLfloat>(0.5f, 0.5f, 1.f),
+		glm::translate<GLfloat>(0.5f, 0.f, 0.f) * glm::scale<GLfloat>(0.5f, 0.5f, 1.f),
+		glm::translate<GLfloat>(0.0f, 0.5f, 0.f) * glm::scale<GLfloat>(0.5f, 0.5f, 1.f),
+		glm::translate<GLfloat>(0.0f, -0.5f, 0.f) * glm::scale<GLfloat>(0.5f, 0.5f, 1.f)
+	};
+	batch.add_objects(matrices);
+	batch.set_attrib_index(0);
 
 	glClear(GL_COLOR_BUFFER_BIT);
-	glDrawArraysInstanced(GL_TRIANGLES, 0, 3, 2);
+	batch.render();
+
 	GLenum err = glGetError();
 	if (err != GL_NO_ERROR){
 		std::cerr << "OpenGL Error: " << std::hex << err << std::dec << "\n";
@@ -85,9 +137,6 @@ int main(int argc, char **argv){
 	SDL_GL_SwapWindow(win);
 	SDL_Delay(1000);
 
-	glDeleteProgram(program);
-	glDeleteBuffers(1, &vbo);
-	glDeleteVertexArrays(1, &vao);
 	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
