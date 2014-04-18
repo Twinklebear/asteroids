bc8f17d374c8f2998c5af66f6c5ce5e9d30f78fb
Will <willusher.life@gmail.com>
2014-04-18 09:14:15 -0700

    I don't think reinterpret cast is what we want either
    I'm not sure on this casting heh

--- test.cpp
+++ test.cpp
@@ -123,8 +123,8 @@ public:
 		assert(i < sz);
 		static_assert(OffsetOf<T, Args...>::value != -1, "Type not in array");
 		size_t offset = OffsetOf<T, Args...>::value;
-		//Note: When interacting with the GL buffer (void*) we'd just use static cast
-		T *t = reinterpret_cast<T*>(&data[0] + offset + i * SizeOf<Args...>::value);
+		//Is there some better way to do this casting? Reinterpret cast might have alignment issues?
+		T *t = static_cast<T*>(static_cast<void*>(&data[0] + offset + i * SizeOf<Args...>::value));
 		return *t;
 	}
 	template<size_t I>
@@ -133,8 +133,8 @@ public:
 		using T = typename TypeAt<I, Args...>::type;
 		static_assert(AlignedOffsetOfIndex<I, 1, T, Args...>::value != -1, "Type not in array");
 		size_t offset = AlignedOffsetOfIndex<I, 1, T, Args...>::value;
-		//Note: When interacting with the GL buffer (void*) we'd just use static cast
-		T *t = reinterpret_cast<T*>(&data[0] + offset + i * SizeOf<Args...>::value);
+		//Is there some better way to do this casting? Reinterpret cast might have alignment issues?
+		T *t = static_cast<T*>(static_cast<void*>(&data[0] + offset + i * SizeOf<Args...>::value));
 		return *t;
 	}
 	size_t size() const {
