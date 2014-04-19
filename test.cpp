0ed44d3d9195edbfccdcc395559758d74e2ff9ae
Will <willusher.life@gmail.com>
2014-04-18 17:45:36 -0700

    Got things cleaned up and working it seems

--- test.cpp
+++ test.cpp
@@ -4,26 +4,6 @@
 #include <type_traits>
 #include <typeinfo>
 
-//Get the total size in bytes of the arguments
-template<typename T, typename... Args>
-struct SizeOf {
-	static const size_t value = sizeof(T) + SizeOf<Args...>::value;
-};
-template<typename T>
-struct SizeOf<T> {
-	static const size_t value = sizeof(T);
-};
-//Get the size in bytes of the arguments with each argument at the desired alignment
-template<size_t Align, typename T, typename... Args>
-struct AlignedSizeOf {
-	static const size_t value = AlignedSizeOf<Align, Args...>::value
-		+ (sizeof(T) % Align == 0 ? sizeof(T) : sizeof(T) + Align - sizeof(T) % Align);
-};
-template<size_t Align, typename T>
-struct AlignedSizeOf<Align, T> {
-	static const size_t value = sizeof(T) % Align == 0 ? sizeof(T) : sizeof(T) + Align - sizeof(T) % Align;
-};
-
 template<int I, typename T, typename... List>
 struct TypeAt {
 	static_assert(I > -1 && I < sizeof...(List) + 1, "TypeAt index out of bounds");
@@ -34,161 +14,60 @@ struct TypeAt<0, T, List...> {
 	using type = T;
 };
 
-//Find the offset by the index
-template<int I, size_t Align, typename T, typename... List>
-struct AlignedOffsetOfIndex {
-	static_assert(I > -1 && I < sizeof...(List) + 1, "AlignedOffsetOfIndex index out of bounds");
-	static const int64_t value = AlignedOffsetOfIndex<I-1, Align, List...>::value != -1 ?
-		AlignedSizeOf<Align, T>::value + AlignedOffsetOfIndex<I-1, Align, List...>::value
-		: -1;
-};
-template<size_t Align, typename T, typename... List>
-struct AlignedOffsetOfIndex<0, Align, T, List...> {
-	static const int64_t value = 0;
-};
-template<size_t Align, typename T>
-struct AlignedOffsetOfIndex<0, Align, T> {
-	static const int64_t value = 0;
-};
-//If I > 0 and there's only one item left in the list the index is out of bounds
-template<int I, size_t Align, typename T>
-struct AlignedOffsetOfIndex<I, Align, T> {
-	//Should there be a static assert here?
-	static const int64_t value = -1;
-};
-
-//Would also need indexed offset of to get offset by index instead of by type
-template<size_t Align, typename T, typename A, typename... List>
-struct AlignedOffsetOf;
-template<bool same, size_t Align, typename T, typename A, typename... List>
-struct AlignedOffsetOfHelper;
-template<size_t Align, typename T, typename A, typename... List>
-struct AlignedOffsetOfHelper<true, Align, T, A, List...> {
-	static const int64_t value = 0;
-};
-template<size_t Align, typename T, typename A, typename... List>
-struct AlignedOffsetOfHelper<false, Align, T, A, List...> {
-	static const int64_t value = AlignedOffsetOf<Align, T, List...>::value != -1 ?
-		AlignedSizeOf<Align, T>::value + AlignedOffsetOf<Align, T, List...>::value : -1;
-};
-template<size_t Align, typename T, typename A>
-struct AlignedOffsetOfHelper<true, Align, T, A> {
-	static const int64_t value = 0;
-};
-template<size_t Align, typename T, typename A>
-struct AlignedOffsetOfHelper<false, Align, T, A> {
-	static const int64_t value = -1;
-};
-//Get the offset in bytes of some type in the list
-template<size_t Align, typename T, typename A, typename... List>
-struct AlignedOffsetOf {
-	static const int64_t value = AlignedOffsetOfHelper<std::is_same<T, A>::value, Align, T, A, List...>::value;
-};
-
-template<typename T, typename A, typename... List>
-struct OffsetOf;
-template<bool same, typename T, typename A, typename... List>
-struct OffsetOfHelper;
-template<typename T, typename A, typename... List>
-struct OffsetOfHelper<true, T, A, List...> {
-	static const int64_t value = 0;
-};
-template<typename T, typename A, typename... List>
-struct OffsetOfHelper<false, T, A, List...> {
-	static const int64_t value = OffsetOf<T, List...>::value != -1 ? sizeof(A) + OffsetOf<T, List...>::value : -1;
-};
-template<typename T, typename A>
-struct OffsetOfHelper<true, T, A> {
-	static const int64_t value = 0;
-};
-template<typename T, typename A>
-struct OffsetOfHelper<false, T, A> {
-	static const int64_t value = -1;
-};
-//Get the offset in bytes of some type in the list
-template<typename T, typename A, typename... List>
-struct OffsetOf {
-	static const int64_t value = OffsetOfHelper<std::is_same<T, A>::value, T, A, List...>::value;
-};
-
+//How to handle std140 UBO layout? I guess a hack way to do it would just be
+//to specialize for glm::vec3/mat4 if the sizeof/alignof isn't right for them
 template<typename... List>
-struct TestSize;
-template<typename A, typename B, typename... List>
-struct TestSize<A, B, List...> {
+struct Size;
+template<typename T, typename... List>
+struct Size<T, List...> {
 	//static const size_t size = sizeof(A) + sizeof(B);
 	static void f(size_t prev = 0){
-		std::cout << "A = '" << typeid(A).name() << "', B = '" << typeid(B).name() << "'\n"
-			<< "sizeof(A)=" << sizeof(A) << ", alignof(B)=" << alignof(B) << "\n"
-			<< "prev size=" << prev << "\n";
-		prev += sizeof(A);
+		prev += sizeof(T);
 		std::cout << "Unpadded size=" << prev << ", ";
-		prev = prev % alignof(B) == 0 ? prev
-			: prev + alignof(B) - prev % alignof(B);
+		prev = prev % alignof(T) == 0 ? prev
+			: prev + alignof(T) - prev % alignof(T);
 		std::cout << "Size with possible padding=" << prev << "\n\n";
-		TestSize<B, List...>::f(prev);
+		Size<List...>::f(prev);
 	}
 };
-template<typename A, typename B>
-struct TestSize<A, B> {
+template<typename T>
+struct Size<T> {
 	static void f(size_t prev = 0){
-		std::cout << "A = '" << typeid(A).name() << "', B = '" << typeid(B).name() << "'\n"
-			<< "sizeof(A)=" << sizeof(A) << ", alignof(B)=" << alignof(B) << "\n"
-			<< "prev size=" << prev << "\n";
-		prev += sizeof(A);
+		prev += sizeof(T);
 		std::cout << "Unpadded size=" << prev << ", ";
-		prev = prev % alignof(B) == 0 ? prev
-			: prev + alignof(B) - prev % alignof(B);
+		prev = prev % alignof(T) == 0 ? prev
+			: prev + alignof(T) - prev % alignof(T);
 		std::cout << "Size with padding=" << prev << "\n\n";
-		TestSize<B>::f(prev);
-	}
-};
-template<typename A>
-struct TestSize<A> {
-	static void f(size_t prev = 0){
-		std::cout << "A = '" << typeid(A).name() << "'\n"
-			<< "Final size=" << prev + sizeof(A) << "\n\n";
 	}
 };
 
 template<int I, typename... List>
-struct TestOffset;
-template<int I, typename A, typename B, typename... List>
-struct TestOffset<I, A, B, List...> {
-	//static const size_t size = sizeof(A) + sizeof(B);
+struct Offset;
+template<int I, typename T, typename... List>
+struct Offset<I, T, List...> {
+	static_assert(I < sizeof...(List) + 1, "Offset index out of bounds");
 	static void f(size_t prev = 0){
-		std::cout << "A = '" << typeid(A).name() << "', B = '" << typeid(B).name() << "'\n"
-			<< "sizeof(A)=" << sizeof(A) << ", alignof(B)=" << alignof(B) << "\n"
-			<< "prev size=" << prev << "\n";
-		prev += sizeof(A);
+		prev += sizeof(T);
 		std::cout << "Unpadded size=" << prev << ", ";
-		prev = prev % alignof(B) == 0 ? prev
-			: prev + alignof(B) - prev % alignof(B);
+		prev = prev % alignof(T) == 0 ? prev
+			: prev + alignof(T) - prev % alignof(T);
 		std::cout << "Offset with possible padding=" << prev << "\n\n";
-		TestOffset<I - 1, B, List...>::f(prev);
+		Offset<I - 1, List...>::f(prev);
 	}
 };
-template<int I, typename A, typename B>
-struct TestOffset<I, A, B> {
+template<typename T, typename... List>
+struct Offset<0, T, List...> {
 	static void f(size_t prev = 0){
-		std::cout << "A = '" << typeid(A).name() << "', B = '" << typeid(B).name() << "'\n"
-			<< "sizeof(A)=" << sizeof(A) << ", alignof(B)=" << alignof(B) << "\n"
-			<< "prev size=" << prev << "\n";
-		prev += sizeof(A);
 		std::cout << "Unpadded size=" << prev << ", ";
-		prev = prev % alignof(B) == 0 ? prev
-			: prev + alignof(B) - prev % alignof(B);
-		std::cout << "Offset with padding=" << prev << "\n\n";
-		TestOffset<I - 1, B>::f(prev);
-	}
-};
-template<typename... List>
-struct TestOffset<0, List...> {
-	static void f(size_t prev = 0){
+		prev = prev % alignof(T) == 0 ? prev
+			: prev + alignof(T) - prev % alignof(T);
+		std::cout << "Offset with possible padding=" << prev << "\n\n";
 		std::cout << "Offset of item=" << prev << "\n";
 	}
 };
 
 
+/*
 template<typename... Args>
 class InterleavedArray {
 	size_t sz;
@@ -219,10 +98,11 @@ public:
 		return sz;
 	}
 };
+*/
 
 int main(int argc, char **argv){
-	//This doesn't work for getting the 0th offset
-	TestOffset<1, int, double>::f();
+	Size<double, char, double>::f();
+	Offset<0, int16_t, double>::f();
 
 	using TA = TypeAt<0, float, int, char>::type;
 	using TB = TypeAt<1, float, int, char>::type;
